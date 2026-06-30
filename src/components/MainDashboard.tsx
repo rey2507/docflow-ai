35c5228b19dcf49ca5c2bb771ba6303df9abef0a
rey2507 <reyyanmd89@gmail.com>
2026-06-30 18:03:40 +0530

    fix: responsive QuickActions buttons and DashboardOverview stat cards for mobile

--- src/components/MainDashboard.tsx
+++ src/components/MainDashboard.tsx
@@ -62,7 +62,7 @@ const MainDashboard: React.FC<MainDashboardProps> = ({ onNavigate }) => {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           <div className="lg:col-span-2">
             <SectionContainer spacing="md">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+               <div className="grid grid-cols-1 gap-4">
                 <UploadZone onUpload={handleFileUpload} uploading={uploading} />
                 <QuickActions onUpload={handleFileUpload} onNavigate={onNavigate} uploading={uploading} />
               </div>
